f54dbcdbb4bf7abcf83b5a677676926d8656365a
Mahesh <moholkarmahesh@aca.edu.in>
2023-09-18 11:20:40 +0530

    Modified Environemnt Variables

--- API/StartupConfig/DependencyInjectionExtenstions.cs
+++ API/StartupConfig/DependencyInjectionExtenstions.cs
@@ -3,65 +3,69 @@ using ERPLibrary.Data;
 using ERPLibrary.DatabaseAccess;
 using ERPLibrary.Repository;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
-namespace API.StartupConfig;
-
-public static class DependencyInjectionExtenstions
+namespace API.StartupConfig
 {
-    public static void AddStandardServices(this WebApplicationBuilder builder)
+    public static class DependencyInjectionExtensions
     {
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
-        builder.Services.AddSingleton<IUserData, UserData>();
-        builder.Services.AddSingleton<IUserRepository, UserRepository>();
-    }
-    public static void AddHealthServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddHealthChecks()
-            .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
-    }
+        public static void AddStandardServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+            builder.Services.AddSingleton<IUserData, UserData>();
+            builder.Services.AddSingleton<IUserRepository, UserRepository>();
+        }
 
-    public static void AddAuthServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddAuthorization(opts =>
+        public static void AddHealthServices(this WebApplicationBuilder builder)
         {
-            opts.AddPolicy(PolicyConstants.Admin, policy =>
-            {
-                policy.RequireClaim("Role", "admin");
-            });
-            opts.AddPolicy(PolicyConstants.Teacher, policy =>
-            {
-                policy.RequireClaim("Role", "teacher");
-            });
-            opts.AddPolicy(PolicyConstants.Student, policy =>
-            {
-                policy.RequireClaim("Role", "student");
-            });
-            opts.FallbackPolicy = new AuthorizationPolicyBuilder()
-                .RequireAuthenticatedUser()
-                .Build();
-        });
-        builder.Services.AddAuthentication("Bearer").AddJwtBearer(opts =>
+            builder.Services.AddHealthChecks()
+                .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+        }
+
+        public static void AddAuthServices(this WebApplicationBuilder builder)
         {
-            opts.TokenValidationParameters = new()
+            var authenticationIssuer = Environment.GetEnvironmentVariable("Issuer");
+            var authenticationAudience = Environment.GetEnvironmentVariable("Audience");
+            var authenticationSecretKey = Environment.GetEnvironmentVariable("SecretKey");
+
+            builder.Services.AddAuthorization(opts =>
             {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
-                ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.ASCII.GetBytes(
-                    builder.Configuration.GetValue<string>("Authentication:SecretKey")
+                opts.AddPolicy(PolicyConstants.Admin, policy =>
+                {
+                    policy.RequireClaim("Role", "admin");
+                });
+                opts.AddPolicy(PolicyConstants.Teacher, policy =>
+                {
+                    policy.RequireClaim("Role", "teacher");
+                });
+                opts.AddPolicy(PolicyConstants.Student, policy =>
+                {
+                    policy.RequireClaim("Role", "student");
+                });
+                opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+            builder.Services.AddAuthentication("Bearer").AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = authenticationIssuer,
+                    ValidAudience = authenticationAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.ASCII.GetBytes(authenticationSecretKey)
                     )
-                )
-            };
-        });
+                };
+            });
+        }
     }
-
-
 }
